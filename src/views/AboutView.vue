e5b6543962370d0a44b196f94029833e1025488e
yaasir cheekoory <yaasir1997@gmail.com>
2023-10-18 23:09:55 +0400

    using routerviews

---
+++ src/views/AboutView.vue
@@ -0,0 +1,3 @@
+<template>
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat dolores autem quisquam enim rerum id velit quibusdam maiores quod eveniet.
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from 'vue';
+import carsData from '../components/data.json'
+
+const cars = ref(carsData);
+</script>
+
+<template>
+  <main class="container">
+    <div class="links">
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+    </div>
+    <h1>Cars</h1>
+    <div class="cards">
+      <div v-for="car in cars" class="card">
+        <h2>{{  car.make }}</h2>
+        <p>${{ car.price }}</p>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.links {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+}
+.cards {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  gap: 1rem;
+  flex-wrap: wrap;
+}
+
+.card {
+  border: 1px solid pink;
+}
+</style>
